9c03b0da2975c79af11ef8cd3c19bba3ef6219e2
Johan Sveholm <jsveholm@gmail.com>
2012-07-09 17:56:12 +0900

    whatever

--- app/mailers/error_mailer.rb
+++
@@ -1,7 +0,0 @@
-class ErrorMailer < ActionMailer::Base
-  default from: "from@example.com"
-
-  def error_log
-    mail(:to => "jsveholm@fir.riec.tohoku.ac.jp", :subject => "ldap_reloaded::Error")
-  end
-end
--- spec/mailers/error_mailer_spec.rb
+++
@@ -1,5 +0,0 @@
-require "spec_helper"
-
-describe ErrorMailer do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
