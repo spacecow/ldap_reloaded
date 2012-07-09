26de465626c37369fbf3b6257d06552878041613
Johan Sveholm <jsveholm@gmail.com>
2012-07-09 18:21:45 +0900

    error mailer  view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   include BasicApplicationController
 
   rescue_from Exception do |ex|
-    ErrorMailer.error_log.deliver
+    ErrorMailer.error_log.deliver(ex)
   end
 
   protect_from_forgery
--- app/mailers/error_mailer.rb
+++ app/mailers/error_mailer.rb
@@ -1,7 +1,9 @@
 class ErrorMailer < ActionMailer::Base
   default from: "from@example.com"
 
-  def error_log
+  def error_log(error)
+    @error = error.message
+    @trace = error.backtrace
     mail(:to => "jsveholm@fir.riec.tohoku.ac.jp", :subject => "ldap_reloaded::Error")
   end
 end
