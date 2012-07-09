52f84ef556046a2ec8e035c0a002f76f8af41a72
Johan Sveholm <jsveholm@gmail.com>
2012-07-09 17:51:23 +0900

    whatever

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
   include BasicApplicationController
 
-  rescue_from Exception do |ex|
-    #ErrorMailer.error_log.deliver
-  end
+  #rescue_from Exception do |ex|
+  #  #ErrorMailer.error_log.deliver
+  #end
 
   protect_from_forgery
 
